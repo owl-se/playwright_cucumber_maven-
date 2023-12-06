8c05fae78ca7c14ce03c66dd2f44d56fec5999ed
aleksandr_chilikin <alc-extern@marketlogicsoftware.com>
2023-12-06 17:13:57 +0300

    hooks

--- src/test/java/com/home/ReRunner.java
+++ src/test/java/com/home/ReRunner.java
@@ -1,4 +1,18 @@
 package java.com.home;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"@target/rerun.txt"},
+        glue = {"steps", "hooks"},
+        plugin = {"pretty",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "rerun:target/rerun.txt"
+        }
+)
+
 public class ReRunner {
 }
--- src/test/java/com/home/TestRunner.java
+++ src/test/java/com/home/TestRunner.java
@@ -1,4 +1,18 @@
 package java.com.home;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        glue = {"steps", "hooks"},
+        plugin = {"pretty",
+                  "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                  "rerun:target/rerun.txt"
+        }
+)
+
 public class TestRunner {
 }
