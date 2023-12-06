8b77dfde0dfd905bb99780f9c799492a0112bf01
aleksandr_chilikin <alc-extern@marketlogicsoftware.com>
2023-12-06 17:10:24 +0300

    hooks

---
+++ src/test/java/com/home/TestRunner.java
@@ -0,0 +1,4 @@
+package java.com.home;
+
+public class TestRunner {
+}
---
+++ src/test/java/com/home/hooks/Hooks.java
@@ -0,0 +1,39 @@
+package java.com.home.hooks;
+
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Tracing;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+import java.com.home.factory.DriverFactory;
+import java.com.home.utils.WebActions;
+import java.nio.file.Paths;
+
+public class Hooks {
+    public DriverFactory driverFactory;
+    public Page page;
+
+    @Before
+    public void launchBrowser() {
+        String browserName = WebActions.getProperty("browser");
+        driverFactory = new DriverFactory();
+        page = driverFactory.initDriver(browserName);
+    }
+
+    @After(order = 0)
+    public void quitBrowser() {
+        page.close();
+    }
+
+    @After(order = 1)
+    public void takeScreenShotAndLogs(Scenario scenario) {
+        if (scenario.isFailed()) {
+            String screen = scenario.getName().replace("", "_");
+            byte[] sourcePath = page.screenshot();
+            scenario.attach(sourcePath, "image/png", screen);
+            DriverFactory.context.tracing().stop(new Tracing.StopOptions().setPath(Paths.get("target/" + screen + ".zip")));
+        }
+    }
+}
