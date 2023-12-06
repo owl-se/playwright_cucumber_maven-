5ca7f5bd4861d8e97845df623dff9860394a3ecc
aleksandr_chilikin <alc-extern@marketlogicsoftware.com>
2023-12-06 16:39:59 +0300

    factory done

--- src/main/java/com/home/factory/DriverFactory.java
+++ src/main/java/com/home/factory/DriverFactory.java
@@ -1,9 +1,9 @@
 package java.com.home.factory;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
+import com.microsoft.playwright.*;
+
+import java.com.home.utils.WebActions;
+import java.util.Locale;
 
 public class DriverFactory {
     public Browser browser;
@@ -15,6 +15,36 @@ public class DriverFactory {
 
     public Page initDriver(String browserName) {
         BrowserType browserType = null;
-        boolean headless = Boolean.valueOf()
+        boolean headless = Boolean.valueOf(WebActions.getProperty("headless"));
+        switch (browserName.toLowerCase(Locale.ROOT)) {
+            case "firefox":
+                browserType = Playwright.create().firefox();
+                browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(headless));
+                break;
+            case "chrome":
+                browserType = Playwright.create().chromium();
+                browser = browserType.launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(headless));
+                break;
+            case "webkit":
+                browserType = Playwright.create().webkit();
+                browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(headless));
+                break;
+            default:
+                throw new IllegalStateException("unknown browser type");
+        }
+        context = browser.newContext();
+        context.tracing().start(new Tracing.StartOptions().setScreenshots(true).setSnapshots(true).setSources(false));
+        page = context.newPage();
+        threadLocalDriver.set(page);
+        threadLocalContext.set(context);
+        return page;
+    }
+
+    public static synchronized Page getPage() {
+        return threadLocalDriver.get();
+    }
+
+    public static synchronized BrowserContext getContext() {
+        return threadLocalContext.get();
     }
 }
--- src/main/java/com/home/utils/WebActions.java
+++ src/main/java/com/home/utils/WebActions.java
@@ -1,9 +1,69 @@
 package java.com.home.utils;
 
+import com.microsoft.playwright.Locator;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Properties;
+
 public class WebActions {
 
     public static String getProperty(String key) {
-        ConfigReader
+        ConfigReader configReader = new ConfigReader();
+        Properties properties = configReader.initProp();
+        return properties.getProperty(key);
+    }
 
+    public static boolean waitUntilElementDisplayed(Locator locator, int timeoutSec) {
+        boolean elementVisible = locator.isVisible();
+        int timer = 0;
+        while (!elementVisible && timer < timeoutSec) {
+            try {
+                Thread.sleep(1000);
+                elementVisible = locator.isVisible();
+                timer++;
+
+            } catch (Exception e) {
+                System.out.println(locator + "was not visible.");
+            }
+        }
+        return elementVisible;
+    }
+
+    public static void encrypt() {
+        try {
+            byte[] encodedBytes = Base64.getEncoder().encode("yourPassword".getBytes("UTF-8"));
+            String encodedValue = new String(encodedBytes);
+        } catch (Exception e) {
+            System.out.println("Password was not Encrypted.");
+        }
+    }
+
+    public static String decrypt(String passwordField) {
+        String encodedBytes = WebActions.getProperty(passwordField);
+        String decodedString = "";
+        try {
+            byte[] decodedBytes = Base64.getDecoder().decode(encodedBytes);
+            decodedString = new String(decodedBytes);
+        } catch (Exception e) {
+            System.out.println("Password was nor Decrypted.");
+        }
+        return decodedString;
+    }
+
+    public static String getRowColValue(String filePath, String sheetName, int rowNum, int colNum) {
+        String fileName = System.getProperty("user.dir") + "\\src\\main\\java\\utils\\functional\\" + filePath + ".xlsx";
+        Workbook workbook = null;
+        try {
+            workbook = WorkbookFactory.create(new File(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Sheet sheet = workbook.getSheet(sheetName);
+        return sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
     }
 }
