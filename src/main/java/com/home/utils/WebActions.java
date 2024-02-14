a228015c4efbc80a93ff4ab69539e1c58e9155d7
aleksandr_chilikin <alc-extern@marketlogicsoftware.com>
2024-02-14 18:13:41 +0300

    gradle working

--- src/main/java/com/home/utils/WebActions.java
+++ src/main/java/com/home/utils/WebActions.java
@@ -1,9 +1,6 @@
 package com.home.utils;
 
 import com.microsoft.playwright.Locator;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -55,15 +52,15 @@ public class WebActions {
         return decodedString;
     }
 
-    public static String getRowColValue(String filePath, String sheetName, int rowNum, int colNum) {
-        String fileName = System.getProperty("user.dir") + "\\src\\main\\java\\utils\\functional\\" + filePath + ".xlsx";
-        Workbook workbook = null;
-        try {
-            workbook = WorkbookFactory.create(new File(fileName));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        Sheet sheet = workbook.getSheet(sheetName);
-        return sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
-    }
+//    public static String getRowColValue(String filePath, String sheetName, int rowNum, int colNum) {
+//        String fileName = System.getProperty("user.dir") + "\\src\\main\\java\\utils\\functional\\" + filePath + ".xlsx";
+//        Workbook workbook = null;
+//        try {
+//            workbook = WorkbookFactory.create(new File(fileName));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        Sheet sheet = workbook.getSheet(sheetName);
+//        return sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
+//    }
 }
--- src/test/java/com/home/steps/StepContext.java
+++ src/test/java/com/home/steps/StepContext.java
@@ -1,7 +1,5 @@
 package com.home.steps;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.HashMap;
 import java.util.Map;
 
